a324a1c978f7d12c6d89de2b817f9ebe66736704
Ingrid-Regina Vähi <ingrid.v2hi@gmail.com>
2021-07-15 20:31:07 +0300

    FEATURE: adding views

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,10 +1,7 @@
 <template>
-    <v-header />
     <div class="l-container">
-        <v-progress />
         <v-button variation="primary" text="Click me" />
         <v-button variation="secondary" text="I'm a button too"/>
-        <v-textmedia imgPosition="right" heading="Lorem ipsum dolor" content="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." imageName="placeholder.png" />
     </div>
 </template>
 <script src="./Home.js"></script>
---
+++ src/views/Projects/Projects.vue
@@ -0,0 +1,4 @@
+<template>
+    <div></div>
+</template>
+<script src="./Projects.js"></script>
