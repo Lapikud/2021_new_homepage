c4debb518ef8153f4272ecdea3ae9050094cc0f7
Henri <Henri.Helisma@outlook.com>
2021-07-28 20:58:51 +0300

    join page

---
+++ src/components/InfoCard/InfoCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="mentorCard ? 'c-grid c-grid--mentor' : 'c-grid'">
+    <div>
+      <slot />
+    </div>
+    <img v-if="image !== null" :src="image" />
+    <iframe
+      v-else
+      width="100%"
+      height="100%"
+      src="https://www.youtube-nocookie.com/embed/mUlFEO-wbPc"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+<script src="./InfoCard.js"></script>
+<style lang="scss" src="./InfoCard.scss" scoped></style>
--- src/views/Join/Join.js
+++ src/views/Join/Join.js
@@ -1,3 +1,30 @@
+import InfoCard from '@/components/InfoCard/InfoCard.vue'
+import Button from '@/components/Button/Button.vue'
+import { mentors } from '@/assets/data/mentors.yml'
+
 export default {
-  name: 'Join'
+  name: 'Join',
+  components: { 'v-infocard': InfoCard, 'v-button': Button },
+  data: () => ({
+    mentors: mentors
+  }),
+  methods: {
+    stringify: (text) => {
+      if (typeof text !== 'string') {
+        return Object.values(text).join('<br/> ')
+      }
+      return text
+    },
+    getTeams: (teams) => {
+      return Object.values(['Tiimid:'].concat(teams.join(', '))).join(' ')
+    },
+    getContact: (contacts) => {
+      const output = []
+      Object.values(contacts).forEach((element) => {
+        const out = `${element.name} : ${element.value}`
+        output.push(out)
+      })
+      return output.join('</br>')
+    }
+  }
 }
